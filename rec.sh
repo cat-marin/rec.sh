c04e632080c732d17fecb94f07a84edd79331662
cat <25059103+cat-marin@users.noreply.github.com>
2020-09-07 20:52:40 -0400

    add first iteration of rec.sh :)

---
+++ rec.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# written by cat-marin
+# depends on maim/slop and ffmpeg
+# Copyright © 2020 cat-marin
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# default config
+# DATE=$(date "+%F-%H-%M-%S")
+# FNAME="red-$DATE"
+# start config
+CONFIGDIR="$HOME/.config/rec.sh"
+CONFIG="$CONFIGDIR/config"
+source $CONFIG
+# end config
+
+help_func() {
+cat << EOF
+written by cat-marin (see license within $0)
+  -h  | --help : display this help text
+  -m1 | --mp4  : record selection and output to '~/.rec/FNAME.mp4'
+  -g  | --gif  : record selection and output to '~/.rec/FNAME.gif'
+EOF
+}
+
+mp4() {
+    SLOP=$(slop -f '|%wx%h|%x,%y')
+    RES=$(echo $SLOP | cut -d "|" -f 2)
+    XGRAB=$(echo $SLOP | cut -d "|" -f 3)
+    mkdir -p $CONFIGDIR/mp4
+    ffmpeg -video_size $RES -framerate 25 -f x11grab -i :0.0+$XGRAB -c:v libx264 -crf 0 -preset ultrafast $CONFIGDIR/mp4/rec.sh-$DATE.mp4
+    echo "$CONFIGDIR/mp4/rec.sh-$DATE.mp4"
+}
+
+gif() {
+    SLOP=$(slop -f '|%wx%h|%x,%y')
+    RES=$(echo $SLOP | cut -d "|" -f 2)
+    XGRAB=$(echo $SLOP | cut -d "|" -f 3)
+    mkdir -p $CONFIGDIR/gif
+    ffmpeg -video_size $RES -framerate 15 -f x11grab -i :0.0+$XGRAB $CONFIGDIR/gif/rec.sh-$DATE.gif
+    echo "$HOME/.rec/gif/rec.sh-$DATE.gif"
+}
+
+for arg in "$@"; do
+	case $arg in
+		-h|--help)
+			help_func
+		;;
+		-m1|--mp4)
+			mp4
+		;;
+		-g|--gif)
+			gif
+		;;
+	esac
+done
+if [[ "$@" == "" ]]; then
+	help_func
+fi
